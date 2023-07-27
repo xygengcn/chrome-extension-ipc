c6b1c2af524adb531e913a1d06a28251ebc2ba81
谢耀庚(XieYaoGeng)-顺丰科技技术集团 <xieyaogeng@sf-express.com>
2023-07-27 18:35:59 +0800

    feat: 增加content-script回调

--- src/service-worker/index.ts
+++ src/service-worker/index.ts
@@ -1,27 +1,26 @@
-import { ITransfer, ITransferMessage, ITransferPort } from '../typings';
+import { ITransfer, ITransferMessage } from '../typings';
 
 /**
  * 创建service通信，作为中间数据传输层
  */
-export function createServiceWorker(option: { noconsole?: boolean }) {
+export function createServiceWorker(option: { noconsole?: boolean; onContentScript?: (message: ITransferMessage, connect: chrome.runtime.Port, sender: chrome.runtime.MessageSender) => void; onPanel?: (message: any, port: chrome.runtime.Port) => void }) {
   // 日志
   const noconsole = option && option.noconsole;
 
   !noconsole && console.log('[devtools-service-worker] 启动', option);
 
   // 作为content script 与 devtool 通信的桥保存者
-  const connections = new Map();
+  const connections = new Map<string, chrome.runtime.Port>();
 
   /**
    * 收到来自panel面板的数据
    */
-  // @ts-ignore
-  chrome.runtime.onConnect.addListener(function (connectPort: ITransferPort) {
+  chrome.runtime.onConnect.addListener(function (connectPort: chrome.runtime.Port) {
     // log
     !noconsole && console.log('[devtools-service-worker] 开始连接插件', connectPort);
 
     // 创建事件监听
-    const extensionListener = (message: ITransferMessage<{ tabId: string }, 'panel:connect'>, port: ITransferPort) => {
+    const extensionListener = (message: ITransferMessage<{ tabId: string }, 'panel:connect'>, port: chrome.runtime.Port) => {
       !noconsole && console.log(`[devtools-service-worker] extensionListener 收到来自${message.source}的数据`, message, port);
 
       switch (message.action) {
@@ -56,30 +55,34 @@ export function createServiceWorker(option: { noconsole?: boolean }) {
     connectPort.onMessage.addListener(extensionListener);
 
     // 插件断开
-    connectPort.onDisconnect.addListener(function (port: ITransferPort) {
+    connectPort.onDisconnect.addListener(function (port: chrome.runtime.Port) {
       !noconsole && console.log('[devtools-service-worker] 插件断开', port);
       // 移除监听
       connectPort.onMessage.removeListener(extensionListener);
       // 移除通信句柄
-      connections.delete(port.sender?.tab?.id);
+      // @ts-ignore
+      port.sender?.tab?.id && connections.delete(port.sender?.tab?.id);
     });
   });
 
   /**
    * 接收content-script的消息
    */
-  // @ts-ignore
-  chrome.runtime.onMessage.addListener((message: ITransferMessage, sender: ITransferPortSender, sendResponse: Function) => {
+  chrome.runtime.onMessage.addListener((message: ITransferMessage, sender: chrome.runtime.MessageSender) => {
     // 收到来自content-script传过来的数据
     !noconsole && console.log('[devtools-service-worker]  收到来自content-script的数据', message, sender);
+
+    // @ts-ignore
+    const connection = connections.get(sender.tab?.id);
+
+    connection && option.onContentScript?.(message, connection, sender);
 
     // 确定数据是发给service-worker自己的，修改数据，再发送给
     if (message.route === 'service-worker') {
       // 获取通信句柄
-      const connection = connections.get(sender.tab.id);
 
       // 传到panel面板页面
-      const tranfer: ITransfer = {
+      const transfer: ITransfer = {
         message: {
           ...message,
           route: 'panel',
@@ -88,10 +91,10 @@ export function createServiceWorker(option: { noconsole?: boolean }) {
         sender
       };
 
-      !noconsole && console.log('[devtools-service-worker]  传送到插件panel', connection, message, sender, tranfer);
+      !noconsole && console.log('[devtools-service-worker]  传送到插件panel', connection, message, sender, transfer);
 
       // 发送数据到插件
-      connection?.postMessage(tranfer);
+      connection?.postMessage(transfer);
     }
   });
 }
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -16,54 +16,5 @@ export interface ITransferMessage<T = any, K extends string = string> {
  */
 export interface ITransfer<T = any> {
   message: ITransferMessage<T>;
-  sender: ITransferPortSender;
-}
-
-/**
- * 页面句柄
- */
-export interface ITransferPort {
-  id: string;
-  sender: ITransferPortSender;
-  postMessage: (transfer: ITransfer) => void;
-  onMessage: {
-    addListener: (...args: any) => void;
-    removeListener: (...args: any) => void;
-  };
-  onDisconnect: any;
-}
-
-/**
- * 发送者
- */
-export interface ITransferPortSender {
-  documentId: string;
-  documentLifecycle: string;
-  frameId: number;
-  id: string;
-  origin: string;
-  tab: {
-    active: boolean;
-    audible: boolean;
-    autoDiscardable: boolean;
-    discarded: boolean;
-    favIconUrl: string;
-    groupId: number;
-    height: number;
-    highlighted: boolean;
-    id: number;
-    incognito: boolean;
-    index: number;
-    mutedInfo: {
-      muted: boolean;
-    };
-    pinned: boolean;
-    selected: boolean;
-    status: string;
-    title: string;
-    url: string;
-    width: number;
-    windowId: number;
-  };
-  url: string;
+  sender: runtime.MessageSender;
 }
